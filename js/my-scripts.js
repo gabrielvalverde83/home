d0b9416422c85dbfca26b0f5111cf952a4bc1c75
Gabriel Valverde <gabrielvalverde83@hotmail.com>
2020-08-17 10:54:33 -0300

    actualizando

---
+++ js/my-scripts.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+	//abrir y cerrar menu en sitio responsivo ///////////
+
+
+	$('.navbar-toggle').click(function(){
+		$('.header-menu').css('left',0);
+	});
+	
+	$('.menu-cerrar h3').click(function(){
+		$('.header-menu').css('left',-1850);
+	})
+
+	// var ventana_ancho = $(window).width();
+
+	// console.log(ventana_ancho);
+
+	// if(ventana_ancho > 1186){
+	// 	$('.header-menu').css('left',0);
+	// };
+	
+	//boton para subir al inicio ///////////////////
+
+ 	var $boton_top	= $('.to-top');
+ 	$boton_top.hide();
+
+ 	$(window).scroll(function(){
+ 		if ( $(this).scrollTop() > 300 ) {
+ 			$boton_top.fadeIn();
+ 		} else {
+ 			$boton_top.fadeOut();
+ 		}
+ 	});
+
+ 	$boton_top.click(function(event){
+ 		event.preventDefault();
+ 		$('html, body').animate({
+ 			scrollTop:0},1200)
+ 		return false;
+ 	});
+
+});
